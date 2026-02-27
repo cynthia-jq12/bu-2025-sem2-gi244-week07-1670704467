05090cc57a6296bff6f911d14a305b1a0171f38f
cynthia <cxnthiazen12@gmail.com>
2026-02-27 16:44:06 +0700

    Week - 07 - Attendant

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     public float speed = 10f;
+
+    private PlayerController player;
     
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        player = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        GameObject go = GameObject.Find("Player");
-        PlayerController player = go.GetComponent<PlayerController>();
-
         if (player.isGameOver == false)
         {
             transform.Translate(speed * Time.deltaTime * Vector3.left);
+  
         }
     }
 }
